699c9a30a53501ff9ea5dfed577a5994024af70b
Shahzadaahmed <shahzada.ahmed1996@gmail.com>
2022-11-13 19:55:15 +0500

    Last class code updated

---
+++ JavaScript/app.js
@@ -0,0 +1,37 @@
+// alert('Welcome to JS!');
+// alert("JS 1st class1");
+
+// console.log("Welcome to JS");
+
+// var myName = "My name is ahmed";
+
+
+
+// var carName = "I have a bike";
+// console.log(carName);
+// alert(carName);
+
+// var schoolName = "Custom Public";
+// console.log(schoolName);
+
+// var schoolDataType = typeof (schoolName);
+// console.log(schoolDataType);
+
+
+// var num = '1996';
+// console.log(num);
+// console.log(typeof (num));
+
+
+// var numData = 8;
+// console.log(numData);
+// console.log(typeof (numData));
+
+
+
+var add = "2" + "4";
+console.log(add);
+
+
+var cal = 2 + 4;
+console.log(cal);
