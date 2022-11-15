09b257c81c1211495f286217ffb75834d14731ed
Shahzadaahmed <shahzada.ahmed1996@gmail.com>
2022-11-15 20:28:41 +0500

    Class 2 code updated

---
+++ JavaScript/class-2/app.js
@@ -0,0 +1,86 @@
+// var myCompanyName = "systems";
+// var _myCompanyName = "systems";
+// console.log(myCompanyName);
+
+
+// var myName = "ahmed";
+// console.log(@myName);
+
+// var var
+
+
+// var add = 2 + 2;
+// console.log(add);
+
+
+// var addInStr = "2" + "2";
+// console.log(addInStr);
+
+
+// var firstName = "muhammad";
+// var lastName = "ahmed";
+// var fullName = firstName + " " + lastName;
+// console.log(fullName);
+
+
+// var num = 10;
+// var anotherNum = 15 + num;
+// console.log(num + anotherNum);
+
+
+// var reminder = 9 % 3;
+// console.log(reminder);
+
+
+// var equation = (4 + 6) * 2;
+// console.log(equation);
+
+
+// var equation = (2 * 2) + (2 * (2 + 3)) + (4 + 5);
+// console.log(equation);
+
+
+// 4 + ( 2 * 5) + 9
+// 4 + 10 + 9
+
+
+// var a = "a";
+// var b = "b";
+// var c = "c";
+
+// var conc = a + b + c;
+
+// console.log(conc);
+
+
+
+// var userInput = prompt("What is your name?");
+// var favSubject = prompt("What is your fav subject?");
+// console.log(userInput);
+// console.log(favSubject);
+
+// var myName = "ahmed";
+// var anotherName = "boss";
+// console.log(myName);
+// console.log(typeof (myName));
+
+
+// == NOte: value only
+// === value + data type
+
+
+// var myName = "10";
+// var anotherName = 10;
+// var condition = myName === anotherName;
+// console.log(condition);
+
+var registeredName = "ahmed";
+var userInput = prompt("Enter your name");
+
+if (userInput == registeredName) {
+    console.log("Both names are same!");
+}
+
+else {
+    console.log("Both names are not same!");
+}
