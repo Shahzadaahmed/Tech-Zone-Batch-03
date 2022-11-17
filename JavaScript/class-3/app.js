2d9028b74882f6f3fa1a74e70fe34c8512ef510a
Shahzadaahmed <shahzada.ahmed1996@gmail.com>
2022-11-17 20:10:05 +0500

    If else code updated

---
+++ JavaScript/class-3/app.js
@@ -0,0 +1,90 @@
+// var myName = "ahmed";
+// var userInput = prompt("Enter your name?");
+// console.log(userInput, typeof (userInput));
+
+
+// var num = 9;
+// console.log(num, typeof (num));
+
+// if (userInput == myName) {
+//     console.log('Correct name!');
+// }
+
+// else if (userInput == "ali") {
+//     console.log('correct ans!')
+// }
+
+// else {
+//     console.log('Inavlid Name!');
+// };
+
+
+
+// var userInput = prompt("Enter any number!");
+
+// if (userInput >= 18) {
+//     console.log('You are adult!');
+// }
+
+// else {
+//     console.log('Invalid num!');
+// };
+
+
+// var userInput = prompt("Enter your fav subject?");
+
+// if (userInput == "math") {
+//     console.log('fav subject')
+// }
+
+// else if ( userInput == "computer" ) {
+//     console.log('fav subject')
+// }
+
+// else {
+//     console.log('Not a fav subject');
+// };
+
+
+// var userInput = prompt("Enter your fav subject?");
+
+// if ((userInput == "math") || (userInput == "computer") || (userInput == "js")) {
+//     console.log('Fav subject!');
+// }
+
+// else {
+//     console.log('Not a fav subject!');
+// };
+
+// var classTiming = prompt("Enter your class timing?");
+// var trainerName = prompt("Enter your trainer name?");
+
+
+// if ((classTiming == 7) && (trainerName == "ahmed")) {
+//     console.log('Correct Information!');
+// }
+
+// else {
+//     console.log('Invalid Input!');
+// };
+
+// var userInput = prompt("Enter any number");
+
+// if ((userInput >= 18) && (userInput <= 40)) {
+//     console.log('condition true!');
+// }
+
+// else {
+//     console.log('Invalid condition!');
+// };
+
+
+var userInput = prompt("Enter any name");
+
+if (userInput != 'ahmed') {
+    console.log('condition true!');
+}
+
+else {
+    console.log('Invalid condition!');
+};
