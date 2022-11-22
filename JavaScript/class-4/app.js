c9ee1dbf0dab189200d60c3fea80880e5bd61d8b
Shahzadaahmed <shahzada.ahmed1996@gmail.com>
2022-11-22 23:24:30 +0500

    Exercise completed

---
+++ JavaScript/class-4/app.js
@@ -0,0 +1,137 @@
+// Solution # 01 , 02 , 05:
+
+// var num = prompt("Enter any number!");
+
+// if (num % 2 == 0) {
+//     console.log(num, "is even number");
+// }
+
+// else {
+//     console.log(num, "is odd number");
+// };
+
+
+// SOlution # 04
+
+// var userInput = prompt("Enter your name!");
+
+// if (userInput == "ahmed") {
+//     console.log("Names are same!");
+// }
+
+// else {
+//     console.log("Names are different!");
+// };
+
+// var userInput = prompt("Enter any character or number");
+
+// if (userInput > 0) {
+//     console.log(userInput, " is number!");
+// }
+
+// else if ((userInput == "A") || (userInput == "B") || (userInput == "C")) {
+//     console.log(userInput, "is capital char!")
+// }
+
+// else if ( (userInput == "a") || (userInput == "b") || (userInput == "c") ) {
+//     console.log(userInput, "is small char!")
+// }
+
+// else {
+//     console.log('Invalid Input!');
+// };
+
+// Q: 7
+
+// var value1 = prompt("Enter number 1");
+// var value2 = prompt("Enter number 2");
+// var operator = prompt("Enter operator!");
+
+// if (operator == "+") {
+//     var result = Number(value1) + Number(value2);
+//     console.log(result);
+// }
+
+// else if (operator == "-") {
+//     var result = Number(value1) - Number(value2);
+//     console.log(result);
+// }
+
+// else if (operator == "/") {
+//     var result = Number(value1) / Number(value2);
+//     console.log(result);
+// }
+
+
+// 9:
+
+// var year = prompt('Enter any year!');
+
+// if (year % 4 == 0) {
+//     console.log(year, "is a leap year!");
+// }
+
+// else {
+//     console.log(year, "is not a leap year!");
+// };
+
+
+
+// 13:
+// var val1 = prompt("Enter number 1");
+// var val2 = prompt("Enter number 2");
+
+// if (val1 > val2) {
+//     console.log(val1, "is greater than", val2)
+// }
+
+// else if (val2 > val1) {
+//     console.log(val2, "is greater than", val1)
+// }
+
+// else if (val1 == val2) {
+//     console.log("Values are equal!")
+// }
+
+// else {
+//     console.log('Invalid Input!');
+// };
+
+// 14:
+// var userInput = prompt("Enter any number!");
+
+// if ( userInput > 0 ) {
+//     console.log(userInput, "is positive");
+// }
+
+// else if ( userInput < 0 ) {
+//     console.log(userInput, "is negative");
+// }
+
+// else if ( userInput == 0 ) {
+//     console.log("Values are zero")
+// }
+
+// else {
+//     console.log('Invalid Input!');
+// };
+
+// var flag = false;
+// console.log(flag);
+// console.log(typeof (flag));
+
+// var myName = undefined;
+// console.log(myName);
+// console.log(typeof (myName));
+
+// 16:
+
+var data = 10;
+
+if (typeof (data) == "string") {
+    console.log('Data is in string data type!');
+}
+
+else if (typeof (data) == "number") {
+    console.log('Data is in number data type!');
+}
