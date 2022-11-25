29773d2e53a71ccaf5b49c59c1cb05d4cc591ad5
Shahzadaahmed <shahzada.ahmed1996@gmail.com>
2022-11-25 13:52:57 +0500

    Code updated

---
+++ JavaScript/class-5/app.js
@@ -0,0 +1,54 @@
+// var userInput = prompt("Enter any number!");
+// userInput = Number(userInput);
+// console.log(userInput);
+// console.log(typeof (userInput));
+
+
+
+// Note: Arrays...!
+// var animalsArr = ["lion", "cat", "tiger", "mouse", "cow"];
+// console.log(animalsArr);
+
+
+// var mixData = ["ahmed", 10, "computer", true, 3, "ali", undefined];
+// console.log(mixData);
+// console.log(mixData.length);
+
+// var findIndex = mixData[5];
+// console.log(mixData[0]);
+
+
+
+// var name_1 = "ahmed";
+// var name_2 = "ali";
+// var name_3 = "bilal";
+
+// var arr = [name_1, name_2, name_3];
+// arr[1] = "asad";
+// console.log(arr);
+
+
+
+// var animalsArr = ["lion", "cat", "tiger", "mouse", "cow"];
+// animalsArr.push('elephant', "mice");
+// animalsArr.pop();
+// animalsArr.unshift(10, "*")
+// animalsArr.shift();
+// console.log(animalsArr);
+
+// var copyArr = animalsArr.slice(1 , 3);
+// console.log(copyArr);
+
+// var animalsArr = ["lion", "cat", "tiger", "mouse", "cow"];
+// animalsArr.splice(1, 0, "ahmed", "zaid");
+// console.log(animalsArr);
+
+
+var arr = [];
+arr[0] = "ahmed";
+arr[1] = "ali";
+arr[2] = "asad";
+// arr.pop();
+arr.splice(2, 0, "pig", "duck", "emu");
+
+console.log(arr);
