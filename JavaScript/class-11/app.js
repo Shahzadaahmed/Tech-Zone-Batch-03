5d3de3f8f1acff8ead113998193cbb1fb1fd2f16
Shahzadaahmed <shahzada.ahmed1996@gmail.com>
2022-12-15 20:19:38 +0500

    Date topic updated

---
+++ JavaScript/class-11/app.js
@@ -0,0 +1,49 @@
+// var str = "tom";
+// var reverseStr = "";
+
+// for (var i = str.length - 1; i >= 0; i--) {
+//     // console.log(str[i]);
+//     reverseStr = reverseStr + str[i];
+//     // console.log(reverseStr);
+// };
+
+// console.log(reverseStr);
+
+// var str = "a";
+// str = str + "b";
+// console.log(str);
+
+
+
+// var arr = ["a", "h", "m", "e", "d"];
+// // var last = arr.pop();
+// // console.log(last);
+
+// for (var i = 0; i < arr.length; i++) {
+//     // console.log(arr);
+
+//     var lastEl = arr.pop();
+//     arr.unshift(lastEl);
+//     console.log(arr);
+// };
+
+
+
+// var arr = ["ahmed", "mehran", "kashan"];
+// console.log(arr);
+
+// var randomNum = Math.random() * arr.length;
+// randomNum = Math.floor(randomNum);
+// console.log(arr[randomNum], randomNum);
+
+
+// var randomNum = Math.floor(Math.random() * arr.length);
+// console.log(arr[randomNum]);
+
+
+
+// TOPIC Date & Time...!
+
+var rightNow = new Date();
+console.log(rightNow);
+console.log(typeof (rightNow));
---
+++ JavaScript/class-12/app.js
@@ -0,0 +1,59 @@
+var rightNow = new Date();
+// console.log(rightNow);
+// console.log(typeof rightNow);
+
+var days = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+var today = rightNow.getDay();
+// console.log(days[today]);
+
+
+var monthArr = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"];
+var month = rightNow.getMonth();
+// console.log(monthArr[month]);
+
+
+var currentYear = rightNow.getFullYear();
+// console.log(currentYear);
+
+
+var date = rightNow.getDate();
+// console.log(date);
+
+
+var hour = rightNow.getHours();
+// console.log(hour);
+
+var mins = rightNow.getMinutes();
+// console.log(mins);
+
+
+var seconds = rightNow.getSeconds();
+// console.log(seconds);
+
+var time = hour + ":" + mins + ":" + seconds;
+// console.log(time);
+
+var dob = "25 December 2022";
+// console.log(new Date(dob));
+
+
+var time = rightNow.getTime();
+// console.log(time);
+
+
+var currentDate = rightNow.toLocaleDateString();
+// console.log(currentDate);
+// console.log(typeof currentDate);
+
+
+var currentTime = rightNow.toLocaleTimeString();
+// console.log(currentTime);
+// console.log(typeof currentTime);
+
+
+
+// Program...!
+var myYear = 1996;
+var currYear = new Date().getFullYear();
+var yearsOld = currYear - myYear;
+console.log(yearsOld);
